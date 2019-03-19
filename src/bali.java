07607d9234fb9b45516b4d3e5f806af0dc2923ab
ksingh0983 <ksingh1823@gmail.com>
2019-03-19 15:49:17 -0400

    This is my first Java Project

---
+++ src/bali.java
@@ -0,0 +1,11 @@
+
+public class bali {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("My Name is Kulwinder Singh");
+
+	}
+
+}
